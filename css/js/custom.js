8cf330a858a837650b26d3f41983001aeb70a3b9
vamcreedy <107324785+vamcreedy@users.noreply.github.com>
2022-07-01 01:07:25 +0530

    Add files via upload

---
+++ css/js/custom.js
@@ -0,0 +1,9 @@
+$(".carousel").carousel({
+  interval: 3000,
+});
+
+// fackbook  : https://www.facebook.com/profile.php?id=100081341505378
+// instagram : https://www.instagram.com/reddys.club/
+// twitter   : https://twitter.com/Reddys_club
+// linkedin  : https://www.linkedin.com/in/reddy-s-club-27905b240/
+// youtub    : https://www.youtube.com/channel/UCasNzgAnzKs59GV_Tw_ZX2g
